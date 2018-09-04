15b5a58ade0095d7ca6a1e74242a5a4df06d9b1a
Brendan Rielly <brendanrielly@gmail.com>
2018-09-04 18:43:32 -0400

    Redux Flow is setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render (
-	<Router history={browserHistory} >{routes}</Router>,
+	<Provider store={store}>
+		<Router history={browserHistory} >{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 );
