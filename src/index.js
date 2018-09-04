4ad2cf75f01001c7715ae1ea9ba519858c3c4c9c
Brendan Rielly <brendanrielly@gmail.com>
2018-09-04 19:20:18 -0400

    added course list and course row

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render (
 	<Provider store={store}>
